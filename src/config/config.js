780207492ab51b1280d8fe55f29d4650afb59e25
Jasmin Esper <jasmin.09-@live.com.ar>
2024-10-24 00:30:46 -0300

    Añadir .gitignore

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
+
+const mongoURI =
+  "mongodb+srv://jasmin09:Jas21@esperfigueroa-backend2.hjfkr.mongodb.net/";
+
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB conectado");
+    await mongoose.connect(mongoURI);
+    console.log("Conectado a MongoDB Atlas");
   } catch (error) {
-    console.error("Error al conectar a MongoDB", error);
+    console.error("Error al conectar a MongoDB Atlas", error);
     process.exit(1);
   }
 };
